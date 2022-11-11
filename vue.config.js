8f1da17dfbc964121e15be72df8ef98bedf4e0e5
yangyq <yangyq@kfb.cn>
2022-11-11 09:40:55 +0800

    解决冲突

--- vue.config.js
+++ vue.config.js
@@ -206,7 +206,7 @@ module.exports = defineConfig({
      * proxy: 配置多个代理
      */
     devServer: {
-        host: "172.16.21.9",
+        host: "172.16.21.10",
         port: 8003,
         // open: true,
         // https: true,
