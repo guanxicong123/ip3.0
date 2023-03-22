f7c832b374166a96362009ab7fced3ce434bb188
guanxicong <3358147437@qq.com>
2023-03-22 11:36:18 +0800

    fix： 客户端播放曲目输入上万的数时有时是9999有时是数字的前四位 （#4961）

--- src/views/play/components/remote-play-component.vue
+++ src/views/play/components/remote-play-component.vue
@@ -79,7 +79,6 @@
             <el-input-number
               v-model="ruleForm.play_number"
               :min="1"
-              :max="9999"
               :value-on-clear="ruleForm.play_number"
               controls-position="right"
               :disabled="ruleForm.radioVal !== 2"
